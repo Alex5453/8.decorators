89699af4bac67e50322c54af58cb9bd1746abf3a
Alex5453 <130847438+Alex5453@users.noreply.github.com>
2025-04-07 20:30:00 +0300

    Update task.js

--- task.js
+++ task.js
@@ -9,7 +9,6 @@ function cachingDecoratorNew(func) {
     const cachedItem = cache.find(item => item.hash === hash);
 
     if (cachedItem) {
-      console.log("Из кеша: " + cachedItem.result);
       return "Из кеша: " + cachedItem.result;
     }
 
@@ -20,7 +19,6 @@ function cachingDecoratorNew(func) {
       cache.shift(); // удаляем самый старый элемент
     }
 
-    console.log("Вычисляем: " + result);
     return "Вычисляем: " + result;
   };
 }
@@ -53,34 +51,3 @@ function debounceDecoratorNew(func, delay) {
 
   return wrapper;
 }
-
-
-
-// Пример для cachingDecoratorNew
-const sum = (...args) => args.reduce((a, b) => a + b, 0);
-const cachedSum = cachingDecoratorNew(sum);
-
-cachedSum(1, 2, 3); // Вычисляем: 6
-cachedSum(1, 2, 3); // Из кеша: 6
-cachedSum(2, 2, 2);
-cachedSum(3, 2, 2);
-cachedSum(4, 2, 2);
-cachedSum(5, 2, 2);
-cachedSum(6, 2, 2); // удалит первый кеш
-cachedSum(1, 2, 3); // снова вычислит
-
-// Пример для debounceDecoratorNew
-const send = (id) => console.log("Сигнал отправлен", id);
-const debouncedSend = debounceDecoratorNew(send, 2000);
-
-setTimeout(() => debouncedSend(1), 0);
-setTimeout(() => debouncedSend(2), 300);
-setTimeout(() => debouncedSend(3), 900);
-setTimeout(() => debouncedSend(4), 1200);
-setTimeout(() => debouncedSend(5), 2300);
-setTimeout(() => debouncedSend(6), 4400);
-setTimeout(() => debouncedSend(7), 4500);
-setTimeout(() => {
-  console.log("Вызовов выполнено:", debouncedSend.count); // 3
-  console.log("Вызовов всего:", debouncedSend.allCount); // 6
-}, 7000);
